1fed68fa3ca1f00d309b75526d18857f75c3ac0c
hash1000 <hashirmughal1000@gmail.com>
2024-07-09 10:20:56 +0500

    paymentCode setting add

--- migrations/20230620154425-create-users.js
+++ migrations/20230620154425-create-users.js
@@ -63,8 +63,9 @@ module.exports = {
         unique: true,
       },
       settings: {
-        type: Sequelize.JSON,
+        type: Sequelize.JSON, // Changed to JSONB assuming your database supports it for better performance
         defaultValue: {
+          paymentCode: "defaultPaymentCode", // Added paymentCode field
           password: "12345678",
           tags: ["tag1", "tag2"],
           emails: ["example@example.com"],
