60135ba32ef8b3c5a9567a7c7a8b777202735de7
hash1000 <hashirmughal1000@gmail.com>
2024-07-26 14:59:32 -0700

    getUserContacts and findSingleChat

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,28 +1,70 @@
-const express = require('express')
-const router = express.Router()
-const multer = require('multer')
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
 // Configure multer storage
-const storage = multer.memoryStorage()
-const upload = multer({ storage })
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-const authMiddleware = require('../middlewares/authenticate')
-const ChatController = require('../controllers/ChatController')
-const chatController = new ChatController()
+const authMiddleware = require("../middlewares/authenticate");
+const ChatController = require("../controllers/ChatController");
+const chatController = new ChatController();
 
-router.post('/invite', authMiddleware, chatController.inviteRequest.bind(chatController))
-router.post('/cancel-invite', authMiddleware, chatController.inviteCancel.bind(chatController))
-router.post('/request', authMiddleware, chatController.chatRequest.bind(chatController))
-router.get('/', authMiddleware, chatController.getChats.bind(chatController))
-router.post('/:chatId/delete', authMiddleware, chatController.deleteChat.bind(chatController))
-router.get('/:chatId/messages', authMiddleware, chatController.getMessages.bind(chatController))
-router.get('/transactions', authMiddleware, chatController.getUserTransactions.bind(chatController))
+router.get("/", authMiddleware, chatController.getChats.bind(chatController));
+router.post(
+  "/",
+  authMiddleware,
+  chatController.getSingleChat.bind(chatController)
+);
+router.post(
+  "/invite",
+  authMiddleware,
+  chatController.inviteRequest.bind(chatController)
+);
+router.post(
+  "/cancel-invite",
+  authMiddleware,
+  chatController.inviteCancel.bind(chatController)
+);
+router.post(
+  "/request",
+  authMiddleware,
+  chatController.chatRequest.bind(chatController)
+);
+router.post(
+  "/:chatId/delete",
+  authMiddleware,
+  chatController.deleteChat.bind(chatController)
+);
+router.get(
+  "/:chatId/messages",
+  authMiddleware,
+  chatController.getMessages.bind(chatController)
+);
+router.get(
+  "/transactions",
+  authMiddleware,
+  chatController.getUserTransactions.bind(chatController)
+);
 
 // route to send a payment request to a user
-router.post('/request-payment', authMiddleware, chatController.sendPaymentRequest.bind(chatController))
+router.post(
+  "/request-payment",
+  authMiddleware,
+  chatController.sendPaymentRequest.bind(chatController)
+);
 
 // route to send a payment to a user
-router.post('/create-payment', authMiddleware, chatController.sendPayment.bind(chatController))
+router.post(
+  "/create-payment",
+  authMiddleware,
+  chatController.sendPayment.bind(chatController)
+);
 
-router.post('/bulk-forward', authMiddleware, upload.array('files'), chatController.bulkForwardMessages.bind(chatController))
+router.post(
+  "/bulk-forward",
+  authMiddleware,
+  upload.array("files"),
+  chatController.bulkForwardMessages.bind(chatController)
+);
 
-module.exports = router
+module.exports = router;
--- services/ChatService.js
+++ services/ChatService.js
@@ -1,13 +1,18 @@
-const ChatRepository = require('../repositories/ChatRepository')
-const PaymentRepository = require('../repositories/PaymentRepository')
-const UserRepository = require('../repositories/UserRepository')
-const socket = require('../config/socket')
-const InSufficientBalance  = require('../errors/InSufficientBalance')
-const User = require('../models/user')
-const sequelize = require('../config/database')
-const path = require('path')
+const ChatRepository = require("../repositories/ChatRepository");
+const PaymentRepository = require("../repositories/PaymentRepository");
+const UserRepository = require("../repositories/UserRepository");
+const socket = require("../config/socket");
+const InSufficientBalance = require("../errors/InSufficientBalance");
+const User = require("../models/user");
+const sequelize = require("../config/database");
+const path = require("path");
 
-const { PutObjectCommand, S3Client, DeleteObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
+const {
+  PutObjectCommand,
+  S3Client,
+  DeleteObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
 
 const s3Client = new S3Client({
   endpoint: process.env.SPACES_END_POINT, // Find your endpoint in the control panel, under Settings. Prepend "https://".
@@ -15,213 +20,284 @@ const s3Client = new S3Client({
   region: process.env.SPACES_REGION, // Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (for example, nyc3).
   credentials: {
     accessKeyId: process.env.SPACES_ACCESS_KEY, // Access key pair. You can create access key pairs using the control panel or API.
-    secretAccessKey: process.env.SPACES_SECRET // Secret access key defined through an environment variable.
-  }
+    secretAccessKey: process.env.SPACES_SECRET, // Secret access key defined through an environment variable.
+  },
 });
-const { PaymentReceivedNotification, PaymentRequestNotification, NewMessageNotification }=require('../notifications')
+const {
+  PaymentReceivedNotification,
+  PaymentRequestNotification,
+  NewMessageNotification,
+} = require("../notifications");
+const { message } = require("../templates/email/email_verification");
 
 class CartService {
-  constructor () {
-    this.chatRepository = new ChatRepository()
-    this.paymentRepository = new PaymentRepository()
-    this.userRepository = new UserRepository()
+  constructor() {
+    this.chatRepository = new ChatRepository();
+    this.paymentRepository = new PaymentRepository();
+    this.userRepository = new UserRepository();
   }
 
-  async chatRequest (requesterId, requesteeId) {
-    const io = socket.getIO()
+  async chatRequest(requesterId, requesteeId) {
+    const io = socket.getIO();
     // Create a new chat if it doesn't exist already
-    let chat = await this.chatRepository.findChat(requesterId, requesteeId)
+    let chat = await this.chatRepository.findChat(requesterId, requesteeId);
 
     if (!chat) {
-      chat = await this.chatRepository.createChat(requesterId, requesteeId)
+      chat = await this.chatRepository.createChat(requesterId, requesteeId);
     }
 
     // Notify the requested user that someone wants to chat with them
-    io.to(`user-${requesteeId}`).emit('chat request', {
+    io.to(`user-${requesteeId}`).emit("chat request", {
       chatId: chat.id,
-      fromUserId: requesterId
-    })
+      fromUserId: requesterId,
+    });
 
-    return { chatId: chat.id }
+    return { chatId: chat.id };
   }
-  async inviteRequest (requesterId, requesteeId) {
+  async inviteRequest(requesterId, requesteeId) {
     // Create a new chat if it doesn't exist already
-    let chat = await this.chatRepository.findInvite(requesterId, requesteeId)
+    let chat = await this.chatRepository.findInvite(requesterId, requesteeId);
 
     if (!chat) {
-      chat = await this.chatRepository.createInvite(requesterId, requesteeId)
+      chat = await this.chatRepository.createInvite(requesterId, requesteeId);
     }
 
-    return { chatId: chat.id }
+    return { chatId: chat.id };
   }
-  async inviteCancel (requesterId, requesteeId) {
+  async inviteCancel(requesterId, requesteeId) {
     // Create a new chat if it doesn't exist already
-    let chat = await this.chatRepository.findInvite(requesterId, requesteeId)
+    let chat = await this.chatRepository.findInvite(requesterId, requesteeId);
 
     if (!chat) {
-      return  { message: `not sent any invite to this User not found:  ${requesterId}`};
+      return {
+        message: `not sent any invite to this User not found:  ${requesterId}`,
+      };
+    } else {
+      chat = await this.chatRepository.cancelInvite(requesterId, requesteeId);
     }
-    else{
-      chat = await this.chatRepository.cancelInvite(requesterId, requesteeId)
-    }
-    return { chatId: chat }
-  }
-  
-
-  async getChats (userId, page, pageSize) {
-     return await this.chatRepository.getUserChat(userId, page, pageSize)
+    return { chatId: chat };
   }
 
-  async getMessages (chatId, page, pageSize, messageId, userId) {
-    return await this.chatRepository.getMessages(chatId, page, pageSize, messageId, userId)
+  async getChats(userId, page, pageSize) {
+    return await this.chatRepository.getUserChat(userId, page, pageSize);
   }
 
-  async deleteChat (chatId, page, pageSize) {
-    return await this.chatRepository.deleteChat(chatId)
+  async getMessages(chatId, page, pageSize, messageId, userId) {
+    return await this.chatRepository.getMessages(
+      chatId,
+      page,
+      pageSize,
+      messageId,
+      userId
+    );
   }
 
-  async getUserTransactions (userId, specificUserId, from, to) {
-    return await this.chatRepository.getUserTransactions(userId, specificUserId, from, to)
+  async deleteChat(chatId, page, pageSize) {
+    return await this.chatRepository.deleteChat(chatId);
   }
 
-  async sendPaymentRequest (requesterId, requesteeId, amount, user, req) {
+  async getSingleChat(requesterId, requesteeId) {
+    let chat = await this.chatRepository.findSingleChat(
+      requesterId,
+      requesteeId
+    );
+    return {
+      status: chat === null ? "Not Friend" : "Friend",
+      message:
+        chat === null
+          ? `No ${requesteeId} is not a friend of user ${requesterId}`
+          : `${chat.user2Id} is a friend of user ${chat.user1Id}`,
+      chatId: chat,
+    };
+  }
+
+  async getUserTransactions(userId, specificUserId, from, to) {
+    return await this.chatRepository.getUserTransactions(
+      userId,
+      specificUserId,
+      from,
+      to
+    );
+  }
+
+  async sendPaymentRequest(requesterId, requesteeId, amount, user, req) {
     // get chat id if chat exists else create chat
-    let chat = await this.chatRepository.findOrCreateChat(requesterId, requesteeId)
+    let chat = await this.chatRepository.findOrCreateChat(
+      requesterId,
+      requesteeId
+    );
     const { id: chatId } = chat;
 
     // create a payment request
-    const paymentRequest = await this.paymentRepository.createPaymentRequest(requesterId, requesteeId, amount)
+    const paymentRequest = await this.paymentRepository.createPaymentRequest(
+      requesterId,
+      requesteeId,
+      amount
+    );
 
     // create a message
-    const message = await this.chatRepository.createMessage(chatId, requesterId, `Payment request of ${amount}`, paymentRequest.id)
+    const message = await this.chatRepository.createMessage(
+      chatId,
+      requesterId,
+      `Payment request of ${amount}`,
+      paymentRequest.id
+    );
 
     // add the payment request to the message
-    message.dataValues.PaymentRequest = paymentRequest
+    message.dataValues.PaymentRequest = paymentRequest;
 
     // get the socket.io instance and emit the payment request to the chat room
     const io = await req.app.get("io");
-    io.to(`chat-${chatId}`).emit('payment request', message)
+    io.to(`chat-${chatId}`).emit("payment request", message);
 
     // send a notification to the requestee
-    const requesteeUser = await this.userRepository.getById(requesteeId)
+    const requesteeUser = await this.userRepository.getById(requesteeId);
     // await new PaymentRequestNotification(requesteeUser.fcm, message, user).sendNotification();
   }
 
-  async sendPayment (requesterId, requesteeId, amount, user, req) {
+  async sendPayment(requesterId, requesteeId, amount, user, req) {
     // get chat id if chat exists else create chat
-    let chat = await this.chatRepository.findOrCreateChat(requesterId, requesteeId)
+    let chat = await this.chatRepository.findOrCreateChat(
+      requesterId,
+      requesteeId
+    );
     const { id: chatId } = chat;
-    
+
     // create a payment request with status 'paid'
-    const paymentRequest = await this.paymentRepository.createPaymentRequest(requesterId, requesteeId, amount, 'accepted')
+    const paymentRequest = await this.paymentRepository.createPaymentRequest(
+      requesterId,
+      requesteeId,
+      amount,
+      "accepted"
+    );
 
     // create a message
-    const message = await this.chatRepository.createMessage(chatId, requesterId, `Payment of ${amount}`, paymentRequest.id)
+    const message = await this.chatRepository.createMessage(
+      chatId,
+      requesterId,
+      `Payment of ${amount}`,
+      paymentRequest.id
+    );
 
     // add the payment request to the message
-    message.dataValues.PaymentRequest = paymentRequest
+    message.dataValues.PaymentRequest = paymentRequest;
 
     // transfer the amount from the requester to the requestee
     this.transferBalance(requesteeId, requesterId, amount)
-    .then(() => {
-      // Handle success
-    })
-    .catch((error) => {
-      if (error instanceof InSufficientBalance) {
-        return socket.emit('error', 'Not enough balance')
-      }
-    })
+      .then(() => {
+        // Handle success
+      })
+      .catch((error) => {
+        if (error instanceof InSufficientBalance) {
+          return socket.emit("error", "Not enough balance");
+        }
+      });
 
     // get the socket.io instance and emit the payment request to the chat room
     const io = await req.app.get("io");
-    io.to(`chat-${chatId}`).emit('payment', message)
+    io.to(`chat-${chatId}`).emit("payment", message);
 
     // send a notification to the requestee
-    const requesteeUser = await this.userRepository.getById(requesteeId)
+    const requesteeUser = await this.userRepository.getById(requesteeId);
     // await new PaymentReceivedNotification(requesteeUser.fcm, message, user).sendNotification();
-    const transaction = await this.chatRepository.getTransactionById(paymentRequest.id);
+    const transaction = await this.chatRepository.getTransactionById(
+      paymentRequest.id
+    );
     return transaction;
   }
 
-  async transferBalance (fromUserId, toUserId, amount) {
-    const t = await sequelize.transaction()
-  
+  async transferBalance(fromUserId, toUserId, amount) {
+    const t = await sequelize.transaction();
+
     try {
       // Deduct balance from the sender
-      const sender = await User.findByPk(fromUserId, { transaction: t })
+      const sender = await User.findByPk(fromUserId, { transaction: t });
       if (sender.personalWalletBalance >= amount) {
-        sender.personalWalletBalance -= amount
-        await sender.save({ transaction: t })
-  
+        sender.personalWalletBalance -= amount;
+        await sender.save({ transaction: t });
+
         // Add balance to the receiver
-        const receiver = await User.findByPk(toUserId, { transaction: t })
-        receiver.personalWalletBalance += amount
-        await receiver.save({ transaction: t })
-  
+        const receiver = await User.findByPk(toUserId, { transaction: t });
+        receiver.personalWalletBalance += amount;
+        await receiver.save({ transaction: t });
+
         // Commit the transaction
-        await t.commit()
-  
-        console.log(`Successfully transferred ${amount} units.`)
+        await t.commit();
+
+        console.log(`Successfully transferred ${amount} units.`);
       } else {
-        await t.rollback()
-        throw new InSufficientBalance('Insufficient balance for the transfer.', 400)
+        await t.rollback();
+        throw new InSufficientBalance(
+          "Insufficient balance for the transfer.",
+          400
+        );
       }
     } catch (error) {
       // If any error occurs, roll back the transaction
-      await t.rollback()
-      console.error('Error transferring balance:', error)
-      throw error
+      await t.rollback();
+      console.error("Error transferring balance:", error);
+      throw error;
     }
   }
 
-  async bulkForwardMessages (payload, files, userId, recipientId, user, req) {
+  async bulkForwardMessages(payload, files, userId, recipientId, user, req) {
     // get chat id if chat exists else create chat
-    const chat = await this.chatRepository.findOrCreateChat(userId, recipientId)
+    const chat = await this.chatRepository.findOrCreateChat(
+      userId,
+      recipientId
+    );
     const { id: chatId } = chat;
 
     // parse the payload for the files and upload them to the server
-    let fileUrls = []
+    let fileUrls = [];
     if (files && files.length > 0) {
-    fileUrls = await Promise.all(files.map(async file => {
-      console.log(file.originalname)
-      const fileName = `${chatId}-${Date.now()}${path.extname(file.originalname)}`;
-      const params = {
-        Bucket: process.env.SPACES_BUCKET_NAME,
-        Key: fileName,
-        Body: file.buffer
-      }
-      await s3Client.send(new PutObjectCommand(params));
-      return fileName
-    }))
-  }
+      fileUrls = await Promise.all(
+        files.map(async (file) => {
+          console.log(file.originalname);
+          const fileName = `${chatId}-${Date.now()}${path.extname(
+            file.originalname
+          )}`;
+          const params = {
+            Bucket: process.env.SPACES_BUCKET_NAME,
+            Key: fileName,
+            Body: file.buffer,
+          };
+          await s3Client.send(new PutObjectCommand(params));
+          return fileName;
+        })
+      );
+    }
 
     // replace the index with the fileUrl in the payload
-    const modifiedPayload = payload.map(message => {
+    const modifiedPayload = payload.map((message) => {
       if (message.index != null) {
-        message.fileUrl = fileUrls[message.index]
+        message.fileUrl = fileUrls[message.index];
       }
       if (message.thumbnail_index != null) {
         message.settings = {
           ...(message.settings || {}),
-          thumbnail_url: fileUrls[message.thumbnail_index]
+          thumbnail_url: fileUrls[message.thumbnail_index],
         };
       }
-      return message
+      return message;
     });
 
     // create messages
-    const messages = await this.chatRepository.createMessages(chatId, userId, modifiedPayload)
-  
-    const io = await req.app.get("io");
-    messages.forEach(message => {
-      io.to(`chat-${chatId}`).emit('message event', message)
-    })
+    const messages = await this.chatRepository.createMessages(
+      chatId,
+      userId,
+      modifiedPayload
+    );
 
-    const otherUser = await User.findOne({where:{id:recipientId}})
+    const io = await req.app.get("io");
+    messages.forEach((message) => {
+      io.to(`chat-${chatId}`).emit("message event", message);
+    });
+
+    const otherUser = await User.findOne({ where: { id: recipientId } });
     // await new NewMessageNotification(otherUser.fcm,messages[messages.length-1], user).sendNotification();
     return messages;
   }
+}
 
-};
-
-module.exports = CartService
+module.exports = CartService;
