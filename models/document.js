905b1c7b626f71dcfaef2b6c921125ba966aaa7a
hash1000 <hashirmughal1000@gmail.com>
2025-03-09 17:49:29 +0500

    revert

--- models/document.js
+++ models/document.js
@@ -1,38 +1,45 @@
-const { DataTypes } = require('sequelize');
-const db = require('../config/database');
+const { DataTypes } = require("sequelize");
 
-const Document = db.define('Document', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  orderNo: {
-    type: DataTypes.STRING, // Must match Order model's orderNo type
-    allowNull: false,
-    references: {
-      model: 'Order',
-      key: 'orderNo'
+module.exports = (sequelize) => {
+  const Document = sequelize.define(
+    "Document",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      orderNo: {
+        type: DataTypes.STRING, // Must match Order model's orderNo type
+        allowNull: false,
+        references: {
+          model: "Order",
+          key: "orderNo",
+        },
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      document: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      tableName: "documents",
     }
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  document: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  createdAt: {
-    allowNull: false,
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
-  },
-  updatedAt: {
-    allowNull: false,
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
-  }
-}, { tableName: 'documents' });
+  );
 
-module.exports = Document;
+  return Document;
+};
