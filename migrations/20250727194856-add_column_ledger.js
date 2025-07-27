537c914b26e8b131ece8394488ab6676620ba0ce
hash1000 <hashirmugal1000@gmail.com>
2025-07-28 00:57:43 +0500

    balance sheet + income + expense + ledger complete crud fixes1

---
+++ migrations/20250727194856-add_column_ledger.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // Step 1: Add the column first
+    await queryInterface.addColumn("ledger", "description", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    // Step 2: Remove the column
+    await queryInterface.removeColumn("ledger", "description");
+  },
+};
