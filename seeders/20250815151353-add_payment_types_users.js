5674ce1a0ed6b5d6067eda39e1d3b18d596dd833
hash1000 <hashirmugal1000@gmail.com>
2025-08-15 20:21:14 +0500

    migration update +seede add

---
+++ seeders/20250815151353-add_payment_types_users.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const { PaymentTypes } = require("../constants");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const [users] = await queryInterface.sequelize.query(`
+      SELECT id FROM users
+    `);
+
+    for (const user of users) {
+      // Check if user already has payment types
+      const [existing] = await queryInterface.sequelize.query(
+        `SELECT id FROM payment_types WHERE userId = :userId`,
+        { replacements: { userId: user.id } }
+      );
+
+      if (existing.length === 0) {
+        const now = new Date();
+
+        const records = PaymentTypes.map((type) => ({
+          name: type,
+          userId: user.id
+        }));
+
+        await queryInterface.bulkInsert("payment_types", records);
+      }
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("payment_types", null, {});
+  },
+};
