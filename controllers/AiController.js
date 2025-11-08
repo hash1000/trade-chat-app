718e3dd9ce687271e594e4e9eea185535dc6a597
hash1000 <hashirmugal1000@gmail.com>
2025-11-08 07:58:21 +0500

    basic assitent deploy

--- controllers/AiController.js
+++ controllers/AiController.js
@@ -4,7 +4,7 @@ const aiService = new AiService();
 class AiController {
   async AiMessages(req, res) {
     try {
-      const messages = await aiService.AiMessages(req.body.payload);
+      const messages = await aiService.AiMessages(req.body);
       res.status(200).json(messages);
     } catch (error) {
       console.error(error);
