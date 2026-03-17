840fd2b65cd2f343f02a7c8221b978e550baa82f
hash1000 <hashirmughal1000@gmail.com>
2026-03-17 22:16:58 +0500

    authorization admin add

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const ServiceController = require("../controllers/ServiceController");
 const authMiddleware = require("../middlewares/authenticate");
 const checkIntegerParam = require("../middlewares/paramIntegerValidation");
+const authorize = require("../middlewares/authorization");
 
 const serviceController = new ServiceController();
 
 router.get("/", authMiddleware, serviceController.list.bind(serviceController));
 router.get("/:id", authMiddleware, checkIntegerParam("id"), serviceController.getById.bind(serviceController));
-router.post("/", authMiddleware, serviceController.create.bind(serviceController));
-router.put("/:id", authMiddleware, checkIntegerParam("id"), serviceController.update.bind(serviceController));
-router.delete("/:id", authMiddleware, checkIntegerParam("id"), serviceController.delete.bind(serviceController));
+router.post("/", authMiddleware,authorize(["admin"]), serviceController.create.bind(serviceController));
+router.put("/:id", authMiddleware,authorize(["admin"]), checkIntegerParam("id"), serviceController.update.bind(serviceController));
+router.delete("/:id", authMiddleware,authorize(["admin"]), checkIntegerParam("id"), serviceController.delete.bind(serviceController));
 router.post("/:id/teams", authMiddleware, checkIntegerParam("id"), serviceController.addTeam.bind(serviceController));
 router.delete("/:id/teams/:teamId", authMiddleware, checkIntegerParam("id"), checkIntegerParam("teamId"), serviceController.removeTeam.bind(serviceController));
 
